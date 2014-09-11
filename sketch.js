6a5fca6ceb13d82362d824776395ba9e028330f7
dyeun96 <dyeun96@naver.com>
2014-09-11 13:43:36 +0900

    Revert "add"
    This reverts commit 7fe95a5605557732f49c4b38badebdddd6c13f90.

--- sketch.js
+++ sketch.js
@@ -10,3 +10,16 @@ function setup() {
   line(10, 10, 90, 10); // x1, y1, x2, y2
     rect(20, 20, 80, 50); // x, y, width, height
     ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+}function setup() {
+  createCanvas(640, 480);
+
+  // 0; black, 255; white
+  // background(red, green, blue); 
+  background(255,0, 0);
+  stroke(255);
+  noFill();
+
+  line(10, 10, 90, 10); // x1, y1, x2, y2
+    rect(20, 20, 80, 50); // x, y, width, height
+    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+}
