5ebbdc8b1ae278dc4db896169af287e937fd41a1
dyeun96 <dyeun96@naver.com>
2014-09-11 14:39:16 +0900

    kjj

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 function setup() {
   createCanvas(640, 480);
 
-  background(255,0, 0);
+
+  background(255,0, 10);
   stroke(255);
   noFill();
 
   line(10, 10, 90, 10);
     rect(20, 20, 80, 50); 
     ellipse(50, 50, 50, 50); 
+}
+
